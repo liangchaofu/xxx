5581d63d5cc35fced3861448d3069231d54a8b63
liangchaofu <70999087+liangchaofu@users.noreply.github.com>
2022-02-26 02:11:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+rm -rf feeds/packages/net/shadowsocks-libev
+rm -rf feeds/luci/applications/luci-app-shadowsocks-libev
